e56747efafc578f38c21090c93462d7a1fc2cbbe
Ordanis <ordanisanchez@gmail.com>
2017-01-29 16:26:59 -0400

    Add to get and post category endpoint funcionality

--- api/resources/categories.py
+++ api/resources/categories.py
@@ -1,13 +1,23 @@
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, marshal_with
+from ..extensions import db
+from ..models import CategoryModel
+from ..utils.outputs import CATEGORY_FIELDS
 
 
 class Categories(Resource):
 
+    @marshal_with(CATEGORY_FIELDS)
     def get(self, id=None):
-        return {'hey': 1}
+        return CategoryModel.query.all()
 
+    @marshal_with(CATEGORY_FIELDS)
     def post(self):
-        pass
+        category = request.form.to_dict()
+        new_categoty = CategoryModel(**category)
+        db.session.add(new_categoty)
+        db.session.commit()
+        return new_categoty
 
     def put(self):
         pass
