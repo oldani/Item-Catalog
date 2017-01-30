db7407ce088be2ee34cebbcdb42310ddc491b3ca
Ordanis <ordanisanchez@gmail.com>
2017-01-30 16:33:28 -0400

    Fix typo on debug in settings

--- api/settings.py
+++ api/settings.py
@@ -2,7 +2,7 @@ from decouple import config
 
 
 class Base:
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = config("SECRET_KEY", default="kjghlij2hlk35jkl5")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
