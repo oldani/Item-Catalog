cb1654bd26910a4decf8f22fbfec0ee9fd07aa22
Ordanis <ordanisanchez@gmail.com>
2017-01-30 15:41:56 -0400

    Rename database env var

--- api/settings.py
+++ api/settings.py
@@ -5,7 +5,7 @@ class Base:
     DEBUG = True
     SECRET_KEY = config("SECRET_KEY", default="kjghlij2hlk35jkl5")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = config("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
 
 
 class Development(Base):
