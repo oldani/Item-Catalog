bea51626fd06998804f8810a3a9595f07ab234f4
Ordanis <ordanisanchez@gmail.com>
2017-01-30 16:51:23 -0400

    Update category model, name must be unique

--- api/models/category.py
+++ api/models/category.py
@@ -4,7 +4,7 @@ from ..extensions import db
 class Category(db.Model):
 
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(100), unique=True)
 
     def __repr__(self):
         return "Category: {}".format(self.name)
