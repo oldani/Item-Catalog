14364c3eeebfce60f097e7183cf83666b69efd9d
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-03-13 23:15:23 -0400

    Redisign Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,14 @@ import React from 'react';
 export const Navbar = () => {
   return (
     <header>
-      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+      <nav className="navbar navbar-toggleable-md navbar-light bg-faded justify-content-between">
         <a className="navbar-brand" href="#">Item-Catalog</a>
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home</a>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link btn btn-outline-primary" href="#">Sign up</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Sign up</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Login</a>
+            <a className="nav-link btn btn-outline-success" href="#">Login</a>
           </li>
         </ul>
       </nav>
