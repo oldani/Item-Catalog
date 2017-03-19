f08529b08a35fc33e02a4c908f60904d3660944e
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-03-19 16:06:54 -0400

    Add cateogires reducer and apply thunk to the store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,40 @@
-const rootReducer = (state = {}, action) => {
+import { combineReducers } from 'redux';
+import {
+  FETCH_CATEGORIES_START,
+  FETCH_CATEGORIES_SUCCESS,
+  FETCH_CATEGORIES_ERROR,
+} from '../actions';
+
+const categories = (state = {
+  isFetching: false,
+  errors: false,
+  entities: [],
+}, action) => {
   switch (action.type) {
+    case FETCH_CATEGORIES_START:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
+    case FETCH_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        entities: action.entities,
+      };
+    case FETCH_CATEGORIES_ERROR:
+      return {
+        ...state,
+        errors: action.error,
+        errorMsg: action.errorMsg,
+      };
     default:
       return state;
   }
 };
 
+const rootReducer = combineReducers({
+  categories,
+});
 
 export default rootReducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const configureStore = () => {
-  const milddlewares = [];
+  const milddlewares = [thunkMiddleware];
   if (process.env.NODE_ENV !== 'production') {
     milddlewares.push(createLogger());
   }
