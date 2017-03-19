7e4ad0553c0acfe2c6eac5dc6dcbdf8ece4e9683
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-03-19 17:21:13 -0400

    Add args parser to get method for items resource

--- api/resources/items.py
+++ api/resources/items.py
@@ -2,16 +2,22 @@ from flask_restful import Resource, marshal_with
 from ..extensions import db
 from ..models import ItemModel, CategoryModel
 from ..utils.outputs import ITEM_FIELDS
-from ..utils.parsers import item_parser
+from ..utils.parsers import item_parser, item_get_parser
 
 
 class Items(Resource):
 
     @marshal_with(ITEM_FIELDS)
     def get(self, item_id=None):
+        result = None
         if item_id:
-            return ItemModel.query.get_or_404(item_id)
-        return ItemModel.query.all()
+            result = ItemModel.query.get_or_404(item_id)
+        params = item_get_parser.parse_args()
+        if params:
+            if params.get('sort_by'):
+                query = '{sort_by} {order}'.format(**params)
+                result = ItemModel.query.order_by(query).all()
+        return result if result else ItemModel.query.all()
 
     @marshal_with(ITEM_FIELDS)
     def post(self):
