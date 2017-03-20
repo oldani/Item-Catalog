c6afda016f36cad606bfc75741ea1c8dcb43085c
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-03-19 22:22:12 -0400

    Add initial behave of Items

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchLatestItems } from '../actions';
 
-const Items = () => (
-  <div></div>
-);
+class Items extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchLatestItems);
+  }
+  render() {
+    const { entities } = this.props.latestItems;
+    return (
+      <div>
+        <ul>
+          {entities && entities.map(item => (
+            <li key={item.id}>
+              <a href="#">{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+Items.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  latestItems: React.PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  latestItems: state.items.latest,
+});
+
+Items = connect(mapStateToProps)(Items);
 
 
 export default Items;
