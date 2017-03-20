8003743104b3d2c1e56b7b906341737f86124555
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-03-19 22:21:42 -0400

    Add latest, entity and items reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,35 @@
 import { combineReducers } from 'redux';
 import {
-  FETCH_CATEGORIES_START,
-  FETCH_CATEGORIES_SUCCESS,
-  FETCH_CATEGORIES_ERROR,
+  FETCH_CATEGORY_START,
+  FETCH_CATEGORY_SUCCESS,
+  FETCH_CATEGORY_ERROR,
+  FETCH_LASTEST_ITEMS_START,
+  FETCH_LASTEST_ITEMS_ERROR,
+  FETCH_LASTEST_ITEMS_SUCCESS,
+  FETCH_ITEM_START,
+  FETCH_ITEM_ERROR,
+  FETCH_ITEM_SUCCESS,
 } from '../actions';
 
 const categories = (state = {
   isFetching: false,
   errors: false,
+  errorMsg: '',
   entities: [],
 }, action) => {
   switch (action.type) {
-    case FETCH_CATEGORIES_START:
+    case FETCH_CATEGORY_START:
       return {
         ...state,
         isFetching: action.isFetching,
       };
-    case FETCH_CATEGORIES_SUCCESS:
+    case FETCH_CATEGORY_SUCCESS:
       return {
         ...state,
         isFetching: action.isFetching,
         entities: action.entities,
       };
-    case FETCH_CATEGORIES_ERROR:
+    case FETCH_CATEGORY_ERROR:
       return {
         ...state,
         errors: action.error,
@@ -33,8 +40,72 @@ const categories = (state = {
   }
 };
 
+const latest = (state = {
+  isFetching: false,
+  errors: false,
+  errorMsg: '',
+  entities: [],
+}, action) => {
+  switch (action.type) {
+    case FETCH_LASTEST_ITEMS_START:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
+    case FETCH_LASTEST_ITEMS_SUCCESS:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        entities: action.entities,
+      };
+    case FETCH_LASTEST_ITEMS_ERROR:
+      return {
+        ...state,
+        errors: action.error,
+        errorMsg: action.errorMsg,
+      };
+    default:
+      return state;
+  }
+};
+
+const entity = (state = {
+  isFetching: false,
+  errors: false,
+  errorMsg: '',
+  data: {},
+}, action) => {
+  switch (action.type) {
+    case FETCH_ITEM_START:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
+    case FETCH_ITEM_SUCCESS:
+      return {
+        ...state,
+        isFetching: action.isFetching,
+        data: action.entities,
+      };
+    case FETCH_ITEM_ERROR:
+      return {
+        ...state,
+        errors: action.error,
+        errorMsg: action.errorMsg,
+      };
+    default:
+      return state;
+  }
+};
+
+const items = combineReducers({
+  latest,
+  entity,
+});
+
 const rootReducer = combineReducers({
   categories,
+  items,
 });
 
 export default rootReducer;
