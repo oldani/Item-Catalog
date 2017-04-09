5f796fb4b39c0bc38b3ddc20a4c29e121782717d
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-04-09 19:01:26 -0400

    Remove category and item views from main view

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,2 +1,4 @@
-from .main import Main, Category, Item
+from .main import Main
 from .login import Login
+from .category import Category
+from .item import Item
--- app/views/main.py
+++ app/views/main.py
@@ -9,20 +9,6 @@ class Main(FlaskView):
 
     def index(self):
         categories = CategoryModel.query.limit(10).all()
-        items = ItemModel.query.order_by('created').limit(10).all()
+        items = ItemModel.query.order_by('created desc').limit(10).all()
         return render_template('index.html', categories=categories,
                                items=items)
-
-
-class Category(FlaskView):
-
-    def get(self, id):
-        category = CategoryModel.query.get(id)
-        return render_template('category.html', category=category)
-
-
-class Item(FlaskView):
-
-    def get(self, id):
-        item = ItemModel.query.get(id)
-        return render_template('item.html', item=item)
